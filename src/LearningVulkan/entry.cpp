130723ae98282dc2641885946efb8cfea4f9d193
Ben <ben@steadfast.co.nz>
2021-12-30 18:20:51 +1300

    Test window and check number of extensions.

--- src/LearningVulkan/entry.cpp
+++ src/LearningVulkan/entry.cpp
@@ -1,8 +1,38 @@
 
+#include <cinttypes>
+#include <memory>
+#include <iostream>
+
 #include "LibraryWrappers/GLFW.hpp"
 #include "LibraryWrappers/glm.hpp"
 
+
+using GLFWWindowHandle = std::unique_ptr < GLFWwindow, decltype([](GLFWwindow* window) { glfwDestroyWindow(window); }) > ;
+
 int main([[maybe_unused]] const int argc, [[maybe_unused]] const char** argv)
 {
+	glfwInit();
+
+	glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
+	GLFWWindowHandle window{ glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr) };
+
+	uint32_t extension_count{ 0 };
+	vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+
+	std::cout << extension_count << " extensions supported\n";
+
+	glm::mat4 matrix{};
+	glm::vec4 vec{};
+	[[maybe_unused]] auto test = matrix * vec;
+
+	while (!glfwWindowShouldClose(window.get()))
+	{
+		glfwPollEvents();
+	}
+
+	window.reset();
+
+	glfwTerminate();
+
 	return 0;
 }
